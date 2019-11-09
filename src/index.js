78f7e66b241fb13f57172692b3d8a83e7a9b5de9
Kane Ma <kanema20@gmail.com>
2019-11-09 11:30:43 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEl)
+}
+
+if (module.hot) { //hook into webpack api
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render);
+    }) //no need to do full page reload (hot module replacement)
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
